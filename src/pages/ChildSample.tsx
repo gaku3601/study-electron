d8dca7474af28b4fbeca206788dedd5d3248d986
gaku <pro.gaku@gmail.com>
2022-04-03 06:23:24 +0900

    微修正

--- src/pages/ChildSample.tsx
+++ src/pages/ChildSample.tsx
@@ -1,11 +1,9 @@
 import "../App.css";
-import {Outlet} from "react-router-dom";
 
 function ChildSample() {
   return (
       <div className="Sample">
         <h1>child</h1>
-        <Outlet />
       </div>
   );
 }
